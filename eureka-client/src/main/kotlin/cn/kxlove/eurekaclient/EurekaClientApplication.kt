2119b4f889edb349cb0bc5be1333c2f069edf484
happyhaha <happyzhengkaixin@gmail.com>
2017-08-15 18:53:55 +0800

    eureka-client setting finish

--- eureka-client/src/main/kotlin/cn/kxlove/eurekaclient/EurekaClientApplication.kt
+++ eureka-client/src/main/kotlin/cn/kxlove/eurekaclient/EurekaClientApplication.kt
@@ -2,8 +2,10 @@ package cn.kxlove.eurekaclient
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class EurekaClientApplication
 
 fun main(args: Array<String>) {
