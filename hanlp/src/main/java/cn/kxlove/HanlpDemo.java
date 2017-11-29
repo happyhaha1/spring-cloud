2be28d8e46f217cfd2bf7b46c761119964c30c92
郑开心 <happyzhengkaixin@gmail.com>
2017-11-29 13:39:01 +0800

    hanlp

---
+++ hanlp/src/main/java/cn/kxlove/HanlpDemo.java
@@ -0,0 +1,30 @@
+package cn.kxlove;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.Segment;
+import com.hankcs.hanlp.seg.common.Term;
+
+import java.util.List;
+
+/**
+ * <p>HanlpDemo</p>
+ *
+ * @author zhengkaixin
+ * @since 2017-10-25 11:05
+ */
+public class HanlpDemo {
+
+    public static void main(String[] args) {
+        String[] testCase = new String[]{
+                "我在上海林原科技有限公司兼职工作，",
+                "我经常在台川喜宴餐厅吃饭，",
+                "偶尔去地中海影城看电影。",
+        };
+        Segment segment = HanLP.newSegment().enableOrganizationRecognize(true);
+        for (String sentence : testCase)
+        {
+            List<Term> termList = segment.seg(sentence);
+            System.out.println(termList);
+        }
+    }
+}
